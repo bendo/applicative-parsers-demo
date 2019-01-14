d7a02fbe20b4c7e9eba9c956d4da1ac83788e3ca
Catherine Galkina <anais94@yandex.ru>
2019-01-14 21:16:29 +0300

    Added simple interactive Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,27 @@
 module Main where
 
+import Demo.Example
+import Demo.Parser (parseString)
+
+import System.IO
+
 main :: IO ()
-main = putStrLn "hello"
+main = do
+  putStrLn "Enter expressions to parse:"
+  putStrLn "expr      ::= constExpr | binOpExpr | negExpr"
+  putStrLn "const     ::= int"
+  putStrLn "int       ::= digit{digit}"
+  putStrLn "digit     ::= '0' | ... | '9'"
+  putStrLn "binOpExpr ::= '(' expr ' ' binOp ' ' expr ')'"
+  putStrLn "binOp     ::= '+' | '*'"
+  putStrLn "negExpr   ::= '-' expr"
+  go
+  where
+    go = do
+      hSetBuffering stdout NoBuffering -- for putStr to print at once
+      putStr ">>> "
+      str <- getLine
+      case parseString str exprParser of
+        Just e -> putStrLn ("Ok: " ++ show e)
+        _      -> putStrLn "Parse error"
+      go
